c3580c10c57ff1f343b111baeecd623f648a29ba
O Je-Gwan <emperorvv@gmail.com>
2021-05-22 01:52:17 +0900

    chore: yarn 2 , pnp 도입

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+const withBundleAnalyzer = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (process.env.ANALYZE === "true") {
+        const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+        config.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            reportFilename: options.isServer
+              ? "../analyze/server.html"
+              : "./analyze/client.html",
+          })
+        );
+      }
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
+      return config;
+    },
+  });
+};
 
 const nextConfig = {
   env: require("./.env.js"),
